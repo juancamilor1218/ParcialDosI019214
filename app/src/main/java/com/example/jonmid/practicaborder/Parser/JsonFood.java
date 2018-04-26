a502aa52dfe5f0ad5db843151edb032604b8dbea
“juan <juancamilo971218@gmail.com>
2018-04-26 13:46:10 -0500

    on

--- app/src/main/java/com/example/jonmid/practicaborder/Parser/JsonFood.java
+++ app/src/main/java/com/example/jonmid/practicaborder/Parser/JsonFood.java
@@ -13,10 +13,9 @@ public class JsonFood {
     public static List<Food> getData(String content) throws JSONException {
 
         JSONObject jsonData = new JSONObject(content);
-        JSONArray jsonArray = jsonData.getJSONArray("result");
+        JSONArray jsonArray = jsonData.getJSONArray("results");
         List<Food> foodList = new ArrayList<>();
         for (int i = 0; i < jsonArray.length(); i++) {
-
             JSONObject item = jsonArray.getJSONObject(i);
             Food food=new Food();
             food.setTitle(item.getString("title"));
